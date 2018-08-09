a45e849f3e632c7ce16c681fa2d70ebaf19d5ae2
Jakub Kotek <kotek@zerostudio.cz>
2018-08-09 15:29:11 +0200

    rename new page to contributor

--- pages/_city/index.vue
+++ pages/_city/index.vue
@@ -19,7 +19,7 @@
 
         <nuxt-link
           class="p-2 ml-4 text-black border-2 border-black no-underline rounded font-semibold hover:bg-white"
-          to="new"
+          to="contributor"
         >Chci se zapojit
         </nuxt-link>
       </div>
