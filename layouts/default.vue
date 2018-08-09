88a2be7001907ef626e39c0b1c165bccdacb2aa8
Jakub Kotek <kotek@zerostudio.cz>
2018-08-09 17:38:13 +0200

    add github link to footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,7 +5,10 @@
     <footer class="container">
       <p class="font-mono">
         <small>
-          Městský zápisník Svitavy | Made with &#x2764 by <a href="https://www.jakubkotek.cz" rel="noopener nofollow" class="no-underline hover:underline text-black font-bold">Jakub Kotek</a> | &copy; {{ new Date().getFullYear() }}
+          Městský zápisník Svitavy |
+          Made with &#x2764 by <a href="https://www.jakubkotek.cz" rel="noopener nofollow" class="no-underline hover:underline text-black font-bold">Jakub Kotek</a> |
+          &copy; {{ new Date().getFullYear() }} |
+          <a href="https://github.com/natocTo/mestsky-zapisnik" rel="noopener nofollow" class="no-underline hover:underline text-black font-bold">Github repository</a>
         </small>
       </p>
     </footer>
